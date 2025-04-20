c9ac811fac0692672d2599a719a479dd7e3faab0
hudsonchen <hudsonchen33@outlook.com>
2025-04-20 21:02:54 +0100

    minor

--- scripts/local_MoG_sp_house.sh
+++ scripts/local_MoG_sp_house.sh
@@ -1,15 +1,15 @@
-for seed in {21..50}
+for seed in 0
 do
-  for particle_num in 10 30 100
-    do
-    /home/zongchen/miniconda3/envs/mmd_cubature/bin/python support_points.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --step_num 10000
-    done
-  for particle_num in 300
-    do
-        /home/zongchen/miniconda3/envs/mmd_cubature/bin/python support_points.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --step_num 100000
-    done
+#   for particle_num in 10 30 100
+#     do
+#     /home/zongchen/miniconda3/envs/mmd_cubature/bin/python support_points.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --step_num 10000
+#     done
+#   for particle_num in 300
+#     do
+#         /home/zongchen/miniconda3/envs/mmd_cubature/bin/python support_points.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --step_num 100000
+#     done
   for particle_num in 1000
     do
-        /home/zongchen/miniconda3/envs/mmd_cubature/bin/python support_points.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --step_num 300000
+        /home/zongchen/miniconda3/envs/mmd_cubature/bin/python sp.py --seed $seed --particle_num $particle_num --dataset elevators --step_size 1.0 --bandwidth 1.0 --step_num 100000
     done
 done
